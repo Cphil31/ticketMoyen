0d410dc47ad5a6e32321c89f11be04444c691404
Cphil31 <cecilephilippe31@gmail.com>
2020-11-13 12:44:09 +0100

    resultat centré

--- js/app.js
+++ js/app.js
@@ -6,25 +6,27 @@ window.onload = function () {
     let NbClient = document.querySelector("input[name='Nb']");
     let ticketMoyen = 0 ;
     let divResult = document.querySelector(".result");
-
-
-
+    
+    
+    
     //object.onsubmit = function(){myScript};
     
     form.addEventListener("submit",function(e){
         console.log("formulaire soumis");
         console.log(Ca.value);
         console.log(NbClient.value);
-
+        
         console.log(`Chiffre d'affaire = ${Ca.value}`);
         console.log(`Nombre de clients = ${NbClient.value}`);
-
+        
         ticketMoyen = Ca.value/NbClient.value;
         console.log(`Ticket Moyen = ${ticketMoyen}`);
-
-
-        divResult.innerHTML=`Le ticket moyen est de ${ticketMoyen.toFixed(2)} euros `;
-        console.log(divResult);
+        
+        let sum = document.createElement("p");
+        sum.setAttribute("class","text-center");
+        divResult.innerHTML="";
+        sum.innerHTML=`Le ticket moyen est de ${ticketMoyen.toFixed(2)} euros `;
+        divResult.appendChild(sum);
 
         
         
