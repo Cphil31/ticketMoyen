886f080a2e305fd10cc914de912e9566980f553d
Cphil31 <cecilephilippe31@gmail.com>
2020-11-14 23:03:13 +0100

    insert if

--- js/app.js
+++ js/app.js
@@ -18,22 +18,36 @@ window.onload = function () {
         
         console.log(`Chiffre d'affaire = ${Ca.value}`);
         console.log(`Nombre de clients = ${NbClient.value}`);
-        
+
         ticketMoyen = Ca.value/NbClient.value;
         console.log(`Ticket Moyen = ${ticketMoyen}`);
-        
         let sum = document.createElement("p");
         sum.setAttribute("class","text-center");
         divResult.innerHTML="";
+        // si les chiffres rentrés sont 0>x : afficher " les données rentrées doivent etre supérieur a 0 "
 
+        if ( Ca.value <= 0 )
+        {
+            sum.innerHTML=`Le chiffre d'affaire doit être supérieur à 0 `;
+        }
+        if ( NbClient.value <= 0 )
+        {
+            sum.innerHTML=`Le nombre de clients  doit être supérieur à 0 `;
+        }
+        if ( Ca.value <= 0 && NbClient.value <= 0 )
+        {
+            sum.innerHTML=`Le chiffre d'affaire et le nombre de clients  doit être supérieur à 0 `;
+        }
 
-        let res = Math.round(ticketMoyen * 100) / 100
-        console.log(res);
+        if (Ca.value > 0 && NbClient.value > 0 ) {
+
+            let res = Math.round(ticketMoyen * 100) / 100
+            console.log(res);
+            
+            sum.innerHTML=`Le ticket moyen est de ${res} euros `;
+        }
         
-        sum.innerHTML=`Le ticket moyen est de ${res} euros `;
         divResult.appendChild(sum);
-
-        
         
         e.preventDefault();
     })
